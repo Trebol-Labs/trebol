544c1e996501381483ac4fb7caf1e02a465a440b
SIERRA <kamisops@gmail.com>
2026-03-10 13:43:16 -0500

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/trebol' : '',
+  assetPrefix: isProd ? '/trebol/' : '',
 }
 
 export default nextConfig
